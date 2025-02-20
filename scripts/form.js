f47a69895b212cd90fb30f5bc73c25a9417a5717
watoja <jimwattsabraham@gmail.com>
2025-02-20 23:11:19 +0300

    product review form

---
+++ scripts/form.js
@@ -0,0 +1,26 @@
+// Get Dates
+const currentYear = new Date().getFullYear();
+document.getElementById("year").innerHTML = currentYear;
+
+const lastModified = document.lastModified;
+document.getElementById("modification").innerHTML = lastModified;
+
+const products = [
+    { id: "fc-1888", name: "Flux Capacitor", averagerating: 4.5 },
+    { id: "fc-2050", name: "Power Laces", averagerating: 4.7 },
+    { id: "fs-1987", name: "Time Circuits", averagerating: 3.5 },
+    { id: "ac-2000", name: "Low Voltage Reactor", averagerating: 3.9 },
+    { id: "jj-1969", name: "Warp Equalizer", averagerating: 5.0 }
+];
+
+// Populate the product dropdown
+document.addEventListener("DOMContentLoaded", function () {
+    let productSelect = document.getElementById("product");
+
+    products.forEach(product => {
+        let option = document.createElement("option");
+        option.value = product.id;
+        option.textContent = product.name;
+        productSelect.appendChild(option);
+    });
+});
